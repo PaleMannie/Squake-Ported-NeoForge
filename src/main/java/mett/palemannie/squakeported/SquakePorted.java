6b0c491ea1407d3bee79acace19b14b1a64733f7
PaleMannie <einalex44@gmail.com>
2025-11-12 00:05:38 +0100

    Initial Commit

--- src/main/java/mett/palemannie/squakeported/SquakePorted.java
+++ src/main/java/mett/palemannie/squakeported/SquakePorted.java
@@ -5,7 +5,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
 @Mod(SquakePorted.MODID)
@@ -16,7 +15,6 @@ public class SquakePorted {
 
     public SquakePorted(IEventBus modEventBus, ModContainer modContainer) {
 
-        //NeoForge.EVENT_BUS.register(this);
         modContainer.registerConfig(ModConfig.Type.COMMON, SquakeConfig.SPEC);
         modEventBus.addListener(ToggleKeyHandler::registerKeys);
     }
